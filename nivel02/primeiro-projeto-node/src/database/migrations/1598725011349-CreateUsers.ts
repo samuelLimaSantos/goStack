6581a76804f0af16bdcf86177afb7a5f06644dba
Samuel Santos <samuellima280499@gmail.com>
2020-08-30 03:46:24 -0300

    add user table

--- nivel02/primeiro-projeto-node/src/database/migrations/1598725011349-CreateUsers.ts
+++ nivel02/primeiro-projeto-node/src/database/migrations/1598725011349-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1598477339137
-  implements MigrationInterface {
+export default class CreateUsers1598725011349 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,14 +14,19 @@ export default class CreateAppointments1598477339137
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
             isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -40,6 +44,6 @@ export default class CreateAppointments1598477339137
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
---
+++ nivel02/primeiro-projeto-node/src/models/User.ts
@@ -0,0 +1,31 @@
+/* eslint-disable camelcase */
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class Users {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Users;
