81912638ced51d4f2beb9eb9fbfd3e991a171b09
Samuel Santos <samuellima280499@gmail.com>
2020-09-19 23:29:04 -0300

    Updating

--- nivel03/appgobarber/src/pages/SignIn/index.tsx
+++ nivel03/appgobarber/src/pages/SignIn/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Text } from 'react-native';
 
 import { Container } from './styles';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return <Container>
+    <Text>
+      teste 2
+    </Text>
+  </Container>;
 };
 
 export default SignIn;
