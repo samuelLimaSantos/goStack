eab1501d15c36dad29b13d4a7544475f7461c1bc
Samuel Santos <samuellima280499@gmail.com>
2020-09-28 23:37:01 -0300

    Add routes

---
+++ nivel03/appgobarber/src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { RectButtonProperties } from 'react-native-gesture-handler';
+import { Container, ButtonText } from './styles';
+
+interface ButtonProps extends RectButtonProperties {
+  children: string;
+}
+
+// eslint-disable-next-line react/prop-types
+const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Container {...rest}>
+      <ButtonText>{children}</ButtonText>
+    </Container>
+  );
+};
+
+export default Button;
--- nivel03/appgobarber/src/pages/SignIn/index.tsx
+++ nivel03/appgobarber/src/pages/SignIn/index.tsx
@@ -1,14 +1,74 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-import { Container } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
-    <Container>
-      <Image source={logoImg} />
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button onPress={() => console.log('Deu')}>Entrar</Button>
+
+            <ForgotPassword>
+              <ForgotPasswordText
+                onPress={() => {
+                  console.log('Clicou');
+                }}
+              >
+                Esqueci a minha senha
+              </ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <CreateAccountButton
+        onPress={() => {
+          navigation.navigate('SignUp');
+        }}
+      >
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
