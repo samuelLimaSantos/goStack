520df2b858901fd87f8ba305327a88bb6c2a0f2a
Samuel Santos <samuellima280499@gmail.com>
2020-10-10 17:59:24 -0300

    Commit

--- nivel03/appgobarber/src/pages/SignUp/index.tsx
+++ nivel03/appgobarber/src/pages/SignUp/index.tsx
@@ -73,7 +73,7 @@ const SignUp: React.FC = () => {
                   formRef.current?.submitForm();
                 }}
               >
-                Entrar
+                Entraa
               </Button>
             </Form>
           </Container>
