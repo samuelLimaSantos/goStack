9279aefb56ce76c4810449617ce65f57d4af0a4a
Samuel Santos <samuellima280499@gmail.com>
2021-01-22 00:11:38 -0300

    Add tests

--- nivel02/primeiro-projeto-node/src/modules/appointments/services/CreateAppointmentService.ts
+++ nivel02/primeiro-projeto-node/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 /* eslint-disable camelcase */
+import 'reflect-metadata';
 import { startOfHour } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 import AppError from '@shared/errors/AppError';
