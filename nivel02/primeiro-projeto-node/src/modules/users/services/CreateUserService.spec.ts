b43d13a1b637bfbb0c6ea97d4dd7711f40edd6c4
Samuel Santos <samuellima280499@gmail.com>
2021-01-28 23:44:28 -0300

    Add all tests

--- nivel02/primeiro-projeto-node/src/modules/users/services/CreateUserService.spec.ts
+++ nivel02/primeiro-projeto-node/src/modules/users/services/CreateUserService.spec.ts
@@ -1,12 +1,14 @@
 import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakesUserRepository';
+import HashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import CreateUserService from './CreateUserService';
 
 describe('CreateUser', () => {
   it('should be able to create a new appointment', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
+    const hashProvider = new HashProvider();
 
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const createUser = new CreateUserService(fakeUsersRepository, hashProvider);
 
     const user = await createUser.execute({
       name: 'John Doe',
@@ -19,8 +21,9 @@ describe('CreateUser', () => {
 
   it('should not be able to create a new user with same email from another', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
+    const hashProvider = new HashProvider();
 
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const createUser = new CreateUserService(fakeUsersRepository, hashProvider);
 
     await createUser.execute({
       name: 'John Doe',
--- nivel02/primeiro-projeto-node/src/modules/users/services/UpdateUserAvatarService.ts
+++ nivel02/primeiro-projeto-node/src/modules/users/services/UpdateUserAvatarService.ts
@@ -6,6 +6,7 @@ import { inject, injectable } from 'tsyringe';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
 
@@ -19,6 +20,9 @@ export default class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute({ user_id, avatarFilename }: Request): Promise<User> {
@@ -29,15 +33,12 @@ export default class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFilename;
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = filename;
 
     await this.usersRepository.save(user);
 
